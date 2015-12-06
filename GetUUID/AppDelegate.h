074be8503eaa04e8f5bcc062fd6f6810b25f0534
Cloudox <cloudox@foxmail.com>
2015-12-06 15:47:16 +0800

    Initial Commit

---
+++ GetUUID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetUUID
+//
+//  Created by csdc-iMac on 15/12/6.
+//  Copyright (c) 2015年 csdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetUUID/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GetUUID
+//
+//  Created by csdc-iMac on 15/12/6.
+//  Copyright (c) 2015年 csdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
